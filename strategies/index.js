bff8c2b4d8039faa6bc98079a8bf138dc0103d51
Diana Thayer <me@dianathayer.org>
2015-06-05 08:08:26 -0700

    added, but also commented out, imports of new strategies

--- strategies/index.js
+++ strategies/index.js
@@ -4,5 +4,7 @@ var basic = require('./basic');
 var strategies = {};
 _.extend(strategies, basic);
 strategies.random = require('./random');
+// strategies.brain = require('./brain');
+// strategies.cournot = require('./cournot');
 
 module.exports = strategies;
